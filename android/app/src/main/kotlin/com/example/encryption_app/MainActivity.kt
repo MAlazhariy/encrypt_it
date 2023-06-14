592a00c69d2148c7397cf114ddea2cc6049349e8
MAlazhariy <mostafa.maok2000@gmail.com>
2023-06-14 19:06:54 +0300

    android native .

--- android/app/src/main/kotlin/com/example/encryption_app/MainActivity.kt
+++ android/app/src/main/kotlin/com/example/encryption_app/MainActivity.kt
@@ -1,28 +1,14 @@
 package malazhariy.encryptIt
 
-import com.example.encryption_app.ListTileNativeAdFactory
-import io.flutter.embedding.android.FlutterActivity
 import io.flutter.embedding.android.FlutterFragmentActivity
 import io.flutter.embedding.engine.FlutterEngine
-import io.flutter.plugins.googlemobileads.GoogleMobileAdsPlugin
-import androidx.annotation.NonNull
 import io.flutter.plugins.GeneratedPluginRegistrant
-
 
 class MainActivity: FlutterFragmentActivity() {
 
     override fun configureFlutterEngine(flutterEngine: FlutterEngine) {
         super.configureFlutterEngine(flutterEngine)
         GeneratedPluginRegistrant.registerWith(flutterEngine)
-
-//        GoogleMobileAdsPlugin.registerNativeAdFactory(
-//                flutterEngine, "listTile", ListTileNativeAdFactory(context))
     }
-
-//    override fun cleanUpFlutterEngine(flutterEngine: FlutterEngine) {
-//        super.cleanUpFlutterEngine(flutterEngine)
-//
-//        GoogleMobileAdsPlugin.unregisterNativeAdFactory(flutterEngine, "listTile")
-//    }
 
 }
