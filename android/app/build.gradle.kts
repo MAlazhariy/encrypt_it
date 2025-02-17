bfb5148987d2629f87b5ac049761cae4056fba2c
Mostafa Alazhariy <87443208+MAlazhariy@users.noreply.github.com>
2025-02-17 12:18:20 +0200

    Reset java to version 17

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -19,12 +19,12 @@ android {
     compileSdk = flutter.compileSdkVersion
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_1_8
-        targetCompatibility = JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
 
     kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_1_8.toString()
+        jvmTarget = "17"
     }
 
     defaultConfig {
--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -12,8 +12,20 @@ subprojects {
     val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
     project.layout.buildDirectory.value(newSubprojectBuildDir)
 }
+
 subprojects {
     project.evaluationDependsOn(":app")
+    // Force subprojects and plugins to use jvm 17
+    tasks.withType<JavaCompile>().configureEach {
+        sourceCompatibility = JavaVersion.VERSION_17.toString()
+        targetCompatibility = JavaVersion.VERSION_17.toString()
+    }
+    tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile>().configureEach {
+        kotlinOptions {
+            jvmTarget = "17"
+        }
+    }
+    // End force subprojects and plugins to use jvm 17
 }
 
 tasks.register<Delete>("clean") {
